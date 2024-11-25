2f260984b062e7ce36165e653719aa91bafb5e19
G00409592 <G00409592@atu.ie>
2024-11-25 16:21:37 +0000

    Add unit test

--- src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductServiceTest.java
@@ -3,15 +3,21 @@ package ie.atu.week8.projectexercise;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
-    @Autowired
+    @InjectMocks
     private ProductService productService;
-    @Autowired
+    @Mock
     private ProductRepository productRepository;
 
     @BeforeEach
@@ -22,6 +28,7 @@ class ProductServiceTest {
     void testSaveProductForApplyDiscount()
     {
         Product product = new Product(1L, "Laptop", "Expensive", 1500);
+        when(productRepository.save(product)).thenReturn(product);
         Product expectedProduct = productService.saveProduct(product);
         assertEquals(1350, expectedProduct.getPrice());
     }
